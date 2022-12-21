ec3a506b9db5118862435639eb438f87c2781f3c
Dio Lemos <pedrodiogenes@outlook.com>
2022-12-21 02:05:10 -0300

    create header cart button component and its css module file

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,13 +1,13 @@
 import chocolateImage from '../../assets/chocolate.jpg'
 import classes from './Header.module.css'
-
+import HeaderCartButton from './HeaderCartButton'
 const Header = props =>{
     return (<>
         <header className={classes.header}> 
             <h1>
                 Gostosuras
             </h1>
-            <button>Carrinho</button>
+            <HeaderCartButton/>
         </header>
         <div className={classes['main-image']}>
             <img src={chocolateImage} alt="Deliciosos Brigadeiros Gourmet!"/>
