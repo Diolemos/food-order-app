bf33d047b861e57cab97c7b96808dac96e023fec
Dio Lemos <pedrodiogenes@outlook.com>
2022-12-26 16:59:15 -0300

    display items in the cart using context The order 'pedir' button only appears if there are items in the cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,22 +1,22 @@
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css'
+import { useContext } from 'react';
+import CartContext from '../../store/cart-context';
 const Cart = props =>{
-    const cartItems = <ul className={classes['cart-items']}>{[{
-        id: 'm1',
-        name: 'Bolo no Pote sensação',
-        description: 'Chocolate Cicao, morangos frescos',
-        price: 18,
-      }].map(item=><li>{item.name}</li>)}</ul> ;
+    const cartCtx = useContext(CartContext)
+   const totalAmount =` R$${cartCtx.totalAmount.toFixed()}`
+   const hasItems = cartCtx.items.length > 0
+    const cartItems = <ul className={classes['cart-items']}>{cartCtx.items.map(item=><li>{item.name}</li>)}</ul> ;
     
     return(
         <Modal onCloseCart={props.onCloseCart}>
            { cartItems}
             <div className={classes.total} ><span>Total Amount</span>
-                <span>27.5</span>
+                <span>{totalAmount}</span>
             </div>
             <div className={classes.actions}>
                 <button onClick={props.onCloseCart} className={classes['button--alt']}>Fechar</button>
-                <button  className={classes.button}>Pedir</button>
+              {hasItems &&  <button  className={classes.button}>Pedir</button>}
             </div>
         </Modal>
     )
