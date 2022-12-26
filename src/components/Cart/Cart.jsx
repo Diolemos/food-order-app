8b2709237d8612b1be0d556dab8a173c5ec7739c
Dio Lemos <pedrodiogenes@outlook.com>
2022-12-26 18:31:19 -0300

    enables add button inside the cart component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -8,7 +8,9 @@ const Cart = props =>{
    const totalAmount =` R$${cartCtx.totalAmount.toFixed()}`
    const hasItems = cartCtx.items.length > 0
     const cartItemRemoveHandler = id =>{}
-    const cartItemAddHandler = item =>{}
+    const cartItemAddHandler = item =>{
+        cartCtx.addItem({...item,amount:1})
+    }
     
     const cartItems = <ul className={classes['cart-items']}>
         {cartCtx.items.map(item=><CartItem key={item.id} name={item.name} amount={item.amount} price={item.price}
