f6b5b0077aab29ec7f0573edf91518b2f84c2209
Dio Lemos <pedrodiogenes@outlook.com>
2022-12-26 18:52:19 -0300

    enable the subtract item button in the cart component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,7 +7,9 @@ const Cart = props =>{
     const cartCtx = useContext(CartContext)
    const totalAmount =` R$${cartCtx.totalAmount.toFixed()}`
    const hasItems = cartCtx.items.length > 0
-    const cartItemRemoveHandler = id =>{}
+    const cartItemRemoveHandler = id =>{
+        cartCtx.removeItem(id)
+    }
     const cartItemAddHandler = item =>{
         cartCtx.addItem({...item,amount:1})
     }
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -17,7 +17,7 @@ const cartReducer = (state, action) => {
     if(existingCartItem){
        
        const updatedItem = {...existingCartItem,
-        amount: existingCartItem.amount + action.item.amount}
+        amount: existingCartItem.amount + action.item.amount};
         updatedItems = [...state.items];
         updatedItems[existingCartItemIndex]= updatedItem
     }
@@ -26,11 +26,33 @@ const cartReducer = (state, action) => {
     }
     
     
+    
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount
     };
   }
+  if(action.type === 'REMOVE'){
+    
+    const existingCartItemIndex = state.items.findIndex(
+        (item)=>item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex]
+    const updatedTotalAmount = state.totalAmount -  existingItem.price
+    let updatedItems;    
+    if(existingItem.amount === 1){
+        updatedItems = state.items.filter(item=>item.id !== action.id)
+    }else{
+        const updatedItem= {...existingItem, amount: existingItem.amount - 1}
+        updatedItems = [...state.items];
+        updatedItems[existingItem]=updatedItem;
+    }
+    return {
+        items: updatedItems,
+        totalAmount: updatedTotalAmount
+    }
+
+}
   return defaultCartState;
 };
 
