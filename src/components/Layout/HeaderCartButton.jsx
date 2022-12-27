b29d6a72c6597c957ba9bbfb838ab0afa1e2c44d
Dio Lemos <pedrodiogenes@outlook.com>
2022-12-27 00:21:26 -0300

    add font and style variables

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,18 +1,33 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const [buttonAnimation, setButtonAnimation] =  useState(false)
   const cartCtx = useContext(CartContext);
 
   const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
+  const {items} = cartCtx
+  const btnclasses = `${classes.button} ${buttonAnimation? classes.bump:''}`
+  useEffect(()=>{
+    if(items.length == 0){
+        return
+    }
+    setButtonAnimation(true);
+    const timer = setTimeout(()=>{setButtonAnimation(false)},300);
 
+    return ()=>{
+        clearTimeout(timer)
+      }
+
+},[items]);
+ 
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnclasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
