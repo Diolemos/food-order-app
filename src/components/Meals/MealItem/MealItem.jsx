581e5346b82aa34550649927f95d86ec5697f811
Dio Lemos <pedro.devsc@outlook.com>
2023-01-03 22:02:31 -0300

    update styles and fix meal discription

--- src/components/Meals/MealItem/MealItem.jsx
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -15,7 +15,7 @@ const MealItem = props=>{
     return(<li className={classes.meal}>
         <div>
                 <h3>{props.name}</h3>
-            <div className={props.description}>{props.description}</div>
+            <div className={classes.description}>{props.description}</div>
             <div className={props.price}>{price}</div>
             
         </div>
